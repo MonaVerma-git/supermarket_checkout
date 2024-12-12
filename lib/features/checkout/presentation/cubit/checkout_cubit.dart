7218559507030cf334815992e949068d947e2370
Mona Vishnu <mn.monaverma@gmail.com>
2024-12-12 00:24:20 +0000

    Clean architecture implemented

---
+++ lib/features/checkout/presentation/cubit/checkout_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'checkout_state.dart';
+
+class CheckoutCubit extends Cubit<CheckoutState> {
+  CheckoutCubit() : super(const CheckoutState(isLoading: true));
+
+  Future<void> loadInitialData() async {
+    final stableState = state;
+    try {
+      emit(state.copyWith(isLoading: true));
+
+      // TODO your code here
+
+      emit(state.copyWith(isLoading: false));
+    } catch (error) {
+      emit(state.copyWith(error: error.toString()));
+      emit(stableState.copyWith(isLoading: false));
+    }
+  }
+}
